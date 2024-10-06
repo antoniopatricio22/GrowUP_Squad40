62647acdc777248fafa9847f6622b72062141575
Antonio Macena <antonio.patricio@souunit.com.br>
2024-10-06 13:24:16 -0300

    Upload: Projeto Spring Boot para Gestão de Ativos da Compesa

---
+++ compesa/src/main/java/com/squad40/compesa/CompesaApplication.java
@@ -0,0 +1,13 @@
+package com.squad40.compesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompesaApplication.class, args);
+	}
+
+}
---
+++ compesa/src/test/java/com/squad40/compesa/CompesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad40.compesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
