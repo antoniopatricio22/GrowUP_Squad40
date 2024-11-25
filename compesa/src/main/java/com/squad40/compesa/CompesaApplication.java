e10142bee60e382c047c2142ebd2a523be809d6d
antoniopatricio22 <antonio.patricio@souunit.com.br>
2024-11-25 01:24:33 -0300

    fix:main-class

--- compesa/src/main/java/com/squad40/compesa/CompesaApplication.java
+++ compesa/src/main/java/com/squad40/compesa/CompesaApplication.java
@@ -3,20 +3,11 @@ package com.squad40.compesa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import io.github.cdimascio.dotenv.Dotenv;
-
-
 
 @SpringBootApplication
 public class CompesaApplication {
 
- 
     public static void main(String[] args) {
-
-        //Dotenv dotenv = Dotenv.load(); 
-       // System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-       // System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
-
         SpringApplication.run(CompesaApplication.class, args);
 
     }
